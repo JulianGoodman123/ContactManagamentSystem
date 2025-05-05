aa96713ec6885d2c397de27ad64c5f56716cc546
31718001 JG <31718001@petroc.ac.uk>
2025-05-05 22:29:58 +0100

    Added buttons and function (command) prints to test

--- Application.py
+++ Application.py
@@ -31,62 +31,95 @@ mycursor.execute("""
     )
 """)
 
-# Save everything we did and close the connection
+# ----- Save and close -----
 db.commit()
 db.close()
+
+# ----- Functions -----
+
+def populate_list():
+    print("Populate")
+
+def add_item():
+    print("add")
+
+def delete_item():
+    print("del")
+
+def update_item():
+    print("update")
+    
+def clear_text():
+    print("clear")
+
+
 
 # ----- GUI -----
 
 # Create Window Object
 app = Tk()
 
-# Title
+# ----- Title -----
 app.title("Julian's Contact Management App")
 
-# Contact Name
+# ----- Contact Name -----
 contact_name = StringVar()
 contact_label = Label(app, text="Contact Full Name: ", font=('bold', 14), pady=20, padx=10)
 contact_label.grid(row=0, column=0, sticky=W)
 contact_entry = Entry(app, textvariable=contact_name)
 contact_entry.grid(row=0, column=1)
 
-# Contact Age
+# ----- Contact Age -----
 contact_age = StringVar()
 contact_label = Label(app, text="Contact Age: ", font=('bold', 14), pady=20, padx=10)
 contact_label.grid(row=0, column=2, sticky=W)
 contact_entry = Entry(app, textvariable=contact_age)
 contact_entry.grid(row=0, column=3)
 
-# Contacts Contact Number
+# ----- Contacts Contact Number -----
 contact_number = StringVar()
 contact_label = Label(app, text="Contact Number: ", font=('bold', 14), pady=20, padx=10)
 contact_label.grid(row=1, column=0, sticky=W)
 contact_entry = Entry(app, textvariable=contact_number)
 contact_entry.grid(row=1, column=1)
 
-# Contact Address
+# ----- Contact Address -----
 contact_address = StringVar()
 contact_label = Label(app, text="Contact Address: ", font=('bold', 14), pady=20, padx=10)
 contact_label.grid(row=1, column=2, sticky=W)
 contact_entry = Entry(app, textvariable=contact_address)
 contact_entry.grid(row=1, column=3)
 
-# Contact List 
+# ----- Contact Info List -----
+contacts_info = Listbox(app, height=8, width=50)
+contacts_info.grid(row=3, column=0, columnspan=3, rowspan=6, pady=2, padx=20)
+
+# ----- Create Scrollbar -----
+scrollbar = Scrollbar(app)
+scrollbar.grid(row=3, column=3)
+
+contacts_info.configure(yscrollcommand=scrollbar.set)
+scrollbar.configure(command=contacts_info.yview)
+
+# ----- Interactive Buttons -----
+
+create_contact = Button(app, text="Create Contact", width=12, command=add_item)
+create_contact.grid(row=2, column=0, pady=20)
+
+delete_contact = Button(app, text="Delete Contact", width=12, command=delete_item)
+delete_contact.grid(row=2, column=1)
+
+update_contact = Button(app, text="Update Contact", width=12, command=update_item)
+update_contact.grid(row=2, column=2)
+
+clear_input = Button(app, text="Clear Input Text", width=12, command=clear_text)
+clear_input.grid(row=2, column=3)
 
 
-
-
-
-
-
-
-
-
-
-# Window Size
+# ----- Window Size -----
 app.geometry("800x450")
 
-# Program Start
+# ----- Program Start -----
 app.mainloop()
 
 
