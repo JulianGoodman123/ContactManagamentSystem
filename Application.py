22e5b40b140b320894ad1a6ca967b3cbd4f1e905
31718001 JG <31718001@petroc.ac.uk>
2025-05-05 22:05:18 +0100

    Added input and labels for each column per the database

--- Application.py
+++ Application.py
@@ -1,13 +1,18 @@
+# ----- Imports -----
 import mysql.connector
+from tkinter import *
 
-# Connect to the MySQL server
+# ----- Database setup ----- 
+# Created Databse and Table with columns
+
+# ----- Connect to the MySQL server -----
 db = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="root"
 )
 
-# Create a cursor object to communicate with the database 
+# ----- Create a cursor object to communicate with the database -----
 mycursor = db.cursor()
 
 # If the database doesn't exist, create it
@@ -29,4 +34,62 @@ mycursor.execute("""
 # Save everything we did and close the connection
 db.commit()
 db.close()
+
+# ----- GUI -----
+
+# Create Window Object
+app = Tk()
+
+# Title
+app.title("Julian's Contact Management App")
+
+# Contact Name
+contact_name = StringVar()
+contact_label = Label(app, text="Contact Full Name: ", font=('bold', 14), pady=20, padx=10)
+contact_label.grid(row=0, column=0, sticky=W)
+contact_entry = Entry(app, textvariable=contact_name)
+contact_entry.grid(row=0, column=1)
+
+# Contact Age
+contact_age = StringVar()
+contact_label = Label(app, text="Contact Age: ", font=('bold', 14), pady=20, padx=10)
+contact_label.grid(row=0, column=2, sticky=W)
+contact_entry = Entry(app, textvariable=contact_age)
+contact_entry.grid(row=0, column=3)
+
+# Contacts Contact Number
+contact_number = StringVar()
+contact_label = Label(app, text="Contact Number: ", font=('bold', 14), pady=20, padx=10)
+contact_label.grid(row=1, column=0, sticky=W)
+contact_entry = Entry(app, textvariable=contact_number)
+contact_entry.grid(row=1, column=1)
+
+# Contact Address
+contact_address = StringVar()
+contact_label = Label(app, text="Contact Address: ", font=('bold', 14), pady=20, padx=10)
+contact_label.grid(row=1, column=2, sticky=W)
+contact_entry = Entry(app, textvariable=contact_address)
+contact_entry.grid(row=1, column=3)
+
+# Contact List 
+
+
+
+
+
+
+
+
+
+
+
+# Window Size
+app.geometry("800x450")
+
+# Program Start
+app.mainloop()
+
+
+
+# ----- Functionality -----
 
