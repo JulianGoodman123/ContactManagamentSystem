610c5e50a7f18af5932c99c9c7340682a1d4b693
31718001 JG <31718001@petroc.ac.uk>
2025-05-05 20:47:50 +0100

    Added imports for tkinter, updated db columns, added connector database name

--- App.py
+++ App.py
@@ -1,10 +1,16 @@
+# Imports list for application
 import mysql.connector
+import tkinter as tk
+
+from tkinter import ttk, messagebox
+from tkinter import *
 
 # Connect to MySQL
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="root",    
+    passwd="root", 
+    database="contactDB",   
 )
 
 mycursor = db.cursor()
@@ -15,13 +21,12 @@ mycursor.execute("CREATE DATABASE IF NOT EXISTS contactDB")
 # Select the database
 db.database = "contactDB"
 
-# Create a table with the required columns
+# Create a table with the required columns (again only if the database doesn't exist)
 mycursor.execute("""
     CREATE TABLE IF NOT EXISTS contacts (
-        first_name VARCHAR(255) PRIMARY KEY,
-        surname VARCHAR(255),
-        contact_number VARCHAR(255),
+        full_name VARCHAR(255) PRIMARY KEY,
         age INT,
+        contact_number VARCHAR(255),
         address TEXT
     )
 """)
